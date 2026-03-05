f7f41fa4bd36073a3bb51b6325d18e89215d2ad7
Saroj_Sapkota <sarojsapkota16450@gmail.com>
2026-03-05 17:45:57 +0000

    update in sd2 db

--- scafffolding/pg-sd2/sd2-db.sql
+++ scafffolding/pg-sd2/sd2-db.sql
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `test_table` (
   `id` int NOT NULL,
-  `name` varchar(512) NOT NULL
+  `name` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
