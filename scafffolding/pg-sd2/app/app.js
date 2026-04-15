cd1d46de11bb6303898f54dd1714f300adae75b4
Saurabh Agrahari <saurabhagrahari552@gmail.com>
2026-04-16 00:57:06 +0100

    Update app.js

--- scafffolding/pg-sd2/app/app.js
+++ scafffolding/pg-sd2/app/app.js
@@ -1,4 +1,4 @@
-const path = require("path"); hello from saurabh
+const path = require("path"); 
 const express = require("express");
 const session = require("express-session");
 const flash = require("connect-flash");
